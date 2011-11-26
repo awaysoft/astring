a078ef3358bc1d4f9fd61422e95f8d921f82adc7
Tom <tom@awaysoft.com>
2011-11-26 13:17:53 +0800

    Update Doc

--- astring.h
+++ astring.h
@@ -1,3 +1,12 @@
+/**
+ * @file astring.h
+ * @brief AString 
+ * @author Tom
+ * @date 2011-11-25
+ * @version 0.2
+ * @note
+ * A AString is an object that handles the memory management of a C string for you. You can think of it as similar to a Java StringBuffer. In addition to the string itself, GString stores the length of the string, so can be used for binary data with embedded nul bytes. To access the C string managed by the AString string, simply use string->str.
+ */
 #ifndef __ASTRING_H__
 #define __ASTRING_H__
 
@@ -6,143 +15,428 @@
 #include "atype.h"
 
 #define A_STRING_FLAG       101
-#define DEFAULT_SIZE        100
 #define ZERO                '\0'
-#define TRUE                1
-#define FALSE               0
 
+/**
+ * @brief A define.
+ * Check if a variable is an AString
+ */ 
 #define A_IS_STRING(string) (*(auchar *)string == A_STRING_FLAG)
 
-typedef struct _AString{
-	auchar              flag; /* Check is a_string */
-	volatile aboolean   lock;
+/**
+ * @brief The Main Struct
+ *
+ * The AString struct contains the public fields of a AString.
+ */ 
+struct _AString{
+	auchar              flag; /**< Don't Change it, it used to check a object is AString. */
+	volatile aboolean   lock; /**< Don't Change it, it used to keep thread safe. */
 
-	char *              str;
-	asize               len;
-	asize               allocated_len;
-}AString;
+	asize               len;  /**< contains the length of the string, not including the terminating nul byte. */
+	asize               allocated_len;   /**< contains the length of the string. */
+	
+	char *              str;  /**< points to the character data. */
+};
 
-typedef struct _AStringArray{
-	AString **          data;
-	asize               size;
-}AStringArray;
+/**
+ * @brief AStringArray Struct.
+ * 
+ * The AStringArray struct contains the public fields of a AStringArray.It only used in a_string_split(). 
+ */ 
+struct _AStringArray{
+	AString **          data; /**< points to the AString data. */
+	asize               size; /**< contains the length of the stringarray */
+};
 
-/* Alloc a new AString */
+/**
+ * @brief Main Struct
+ */
+typedef struct _AString AString;
+/**
+ * @brief the array of some AString
+ */
+typedef struct _AStringArray AStringArray;
+
+/**
+ * @brief Create a new AString, initialized with the given string.
+ * @param init the initial text to copy into the string.
+ * @return a new AString
+ * @since 0.1
+ */ 
 AString *      a_string_new                   (const char *init);
+
+/**
+ * @brief Create a new AString with len length.
+ *  if strlen(init) is larger than len, the length of new string will be len, otherwise the length will be strlen(init)
+ * @param init the initial text to copy into the string.
+ * @param len the max length
+ * @return a new AString
+ * @since 0.1
+ */ 
 AString *      a_string_new_len               (const char *init, 
-						                      asize len);
+                                              asize len);
+
+/**
+ * @brief Create a new AString with size allocated_len
+ *  This is useful if you are going to add a lot of text to the string and don't want it to be reallocated too often.
+ * @param size the default size of the space allocated to hold the string
+ * @return a new AString
+ * @since 0.1
+ */
 AString *      a_string_sized_new             (asize size);
+/**
+ * @brief Copies the bytes from a string into an AString, destroying any previous contents. It is rather like the standard strcpy() function, except that you do not have to worry about having enough space to copy the string.
+ * @param string the destination AString. Its current contents are destroyed.
+ * @param value the string to copy into string
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_assign                (AString *string,
-						                      const char * value);
+                                              const char * value);
+/**
+ * @brief duplicate an AString.
+ * @warning You should free the return string when you don't want to use it.
+ * @param source the source AString.
+ * @return the new AString
+ * @since 0.1
+ */
 AString *      a_string_dup                   (AString *source);
+/**
+ * @brief duplicate char * from an AString.
+ * @warning You should free the return string when you don't want to use it.
+ *  When you just want the string to print or view, not change it, you can use source->str instead.
+
+ * @param the source AString.
+ * @return the new AString
+ * @since 0.1
+ */
 char    *      a_string_dupstr                (AString *source);
 
-/* Get String From a file */
-AString *      a_string_get_file_content			 (const char * filename);
-
-/* Append Something */
+/**
+ * @brief Create a new AString from a file.
+ * @warning You should free the return string when you don't want to use it.
+ * @param filename the file you need to read.
+ * @return the new AString
+ * @since 0.1
+ */
+AString *      a_string_get_file_content     (const char * filename);
+/**
+ * @brief Append a string to An AString.
+ * @param string the destination AString
+ * @param value the string you want to append
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_append                (AString *string,
-						                      const char *value);
+                                              const char *value);
+/**
+ * @brief Append a char to An AString.
+ * @param string the destination AString
+ * @param c the char you want to append
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_append_c              (AString *string,
-						                      char c);
+                                              char c);
+/**
+ * @brief Append a char to An AString.
+ * @param string the destination AString
+ * @param wc the wide char you want to append
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_append_unichar        (AString *string,
-					                          aunichar wc);
+                                              aunichar wc);
+/**
+ * @brief Append a char to An AString.
+ * @param string the destination AString
+ * @param value the string you want to append
+ * @param len the max char length to append
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_append_len            (AString *string,
-					                          const char *value,
-					                          asize len);
-
-/* Prepend Something */
+                                              const char *value,
+                                              asize len);
+/**
+ * @brief Prepend a string to An AString.
+ * @param string the destination AString
+ * @param value the string you want to prepend
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_prepend               (AString *string,
-						                      const char *value);
+                                              const char *value);
+/**
+ * @brief Prepend a char to An AString.
+ * @param string the destination AString
+ * @param c the char you want to prepend
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_prepend_c             (AString *string,
-					                          char c);
+                                              char c);
+/**
+ * @brief Prepend a char to An AString.
+ * @param string the destination AString
+ * @param wc the wide char you want to prepend
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_prepend_unichar       (AString *string,
-						                      aunichar wc);
+                                              aunichar wc);
+/**
+ * @brief Prepend a char to An AString.
+ * @param string the destination AString
+ * @param value the string you want to prepend
+ * @param len the max char length to prepend
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_prepend_len           (AString *string,
-						                      const char *value,
-						                      asize len);						
-
-/* Insert Something */
+                                              const char *value,
+                                              asize len);
+/**
+ * @brief Insert a string to An AString.
+ * @param string the destination AString
+ * @param pos the position where do you want to insert
+ * @param value the string you want to insert
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_insert                (AString *string,
-						                      asize pos,
-					                          const char *value);
+                                              asize pos,
+                                              const char *value);
+/**
+ * @brief Insert a char to An AString.
+ * @param string the destination AString
+ * @param pos the position where do you want to insert
+ * @param c the char you want to insert
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_insert_c              (AString *string,
-						                      asize pos,
-						                      char c);
+                                              asize pos,
+                                              char c);
+/**
+ * @brief Insert a char to An AString.
+ * @param string the destination AString
+ * @param pos the position where do you want to insert
+ * @param wc the wide char you want to insert
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_insert_unichar        (AString *string,
-						                      asize pos,
-						                      aunichar wc);
+                                              asize pos,
+                                              aunichar wc);
+/**
+ * @brief Insert a char to An AString.
+ * @param string the destination AString
+ * @param pos the position where do you want to insert
+ * @param value the string you want to insert
+ * @param len the max char length to insert
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_insert_len            (AString *string,
-						                      asize pos,
-						                      const char *value,
-						                      asize len);	
+                                              asize pos,
+                                              const char *value,
+                                              asize len);
+/**
+ * @brief Get the key of the string
+ *  When a string is like key=value,it return key
+ * @warning You should free the return string when you don't want to use it.
+ * @param string the source AString
+ * @return a new string
+ * @since 0.2
+ */
 char *         a_string_get_key               (AString *string);
+/**
+ * @brief Get the value of the string
+ *  When a string is like key=value,it return value
+ * @warning You should free the return string when you don't want to use it.
+ * @param string the source AString
+ * @return a new string
+ * @since 0.2
+ */
 char *         a_string_get_value             (AString *string);
-						
-/* Overwrite Something */
+/**
+ * @brief overwrite an AString from pos
+ * @param string the AString you want to overwrite
+ * @param pos the overwrite position
+ * @param value the string you want to overwrite to
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_overwrite             (AString *string,
                                               asize pos,
                                               const char *value);
+/**
+ * @brief overwrite an AString from pos, and overwrite the most len char
+ * @param string the AString you want to overwrite
+ * @param pos the overwrite position
+ * @param value the string you want to overwrite to
+ * @param len the max char to overwrite
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_overwrite_len         (AString *string,
                                               asize pos,
                                               const char *value,
                                               asize len);
-
-/* Erase Something */
+/**
+ * @brief Erase something from an AString.
+ * @param string the AString you want to erase
+ * @param pos the erase start
+ * @param len How long you want to erase
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_erase                 (AString *string,
-											  asize pos,
-											  asize len);
-											  
-
-/* Truncate Something 
- * Leave the first len char 
- * */
+                                              asize pos,
+                                              asize len);
+/**
+ * @brief Truncate an AString, leave the first len char.
+ * @param string the AString you want to truncate
+ * @param len How long you want to leave
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_truncate              (AString *string,
-											  asize len);
-											  
-											  
-/* Set Size */
+                                              asize len);
+/**
+ * @brief Set string's length
+ *  if len is smaller than string->len, it'll be truncate,else it'll fill with undefined char.
+ * @param string the AString you want to set.
+ * @param len the new length
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_set_size              (AString *string,
-											  asize len);
-
+                                              asize len);
+/**
+ * @brief wipe off the head and tail's space or tab.
+ *  "  abc  " will be "abc"
+ * @param string the AString you want to trim
+ * @return string
+ * @since 0.1
+ */
 AString *      a_string_trim                  (AString *string);
-
+/**
+ * @brief Splits a string into array
+ * @param string the AString you want to split
+ * @param splitstr the split string
+ * @return string
+ * @since 0.2
+ */
 AStringArray * a_string_split                 (AString *string,
-											  const char *splitstr);
-
-/* Get SubString */
-AString *	   a_string_substring			(AString *string, asize start, asize end);
-
-AString *	   a_string_substring_new	    (AString *string, asize start, asize end);
-
-/* Get Chat */
-char		   a_string_get_char				(AString *string, asize position);
-
-AString *	   a_string_set_char				(AString *string, asize position, char ch);
-
-/* Find and Replace */
-int 	       a_string_find              	(AString *string,
-											char *str,
-											asize position);
-
-int		 	  a_string_replace				(AString *string,
-											char *findstr,
-											char *replacestr,
-											asize position);
-
-int 		  a_string_replace_all			(AString *string,
-											char *findstr,
-											char *replacestr);
-
-
-/* Free this string */
+                                              const char *splitstr);
+/**
+ * @brief change an AString to it's substring.
+ * @param string the AString you want to sub
+ * @param start where do you want to start
+ * @param end where do you want to end
+ * @return string
+ *
+ * @note Example:
+ * @note
+ * @note  str = astring_new("01234567890");
+ * @note  
+ * @note  astring_substring(str, 2, 5);
+ * @note  
+ * @note  printf("%s\n", str->str);
+ * @note  
+ * @note  Run:    2345
+ * @since 0.1
+ */
+AString *      a_string_substring             (AString *string, asize start, asize end);
+/**
+ * @brief Like substring, but it'll create a new AString to return.
+ *  string will not to be changed.
+ * @warning You should free the return string when you don't want to use it.
+ * @param string the AString you want to sub
+ * @param start where do you want to start
+ * @param end where do you want to end
+ * @return string
+ * @since 0.1
+ */
+AString *      a_string_substring_new         (AString *string, asize start, asize end);
+/**
+ * @brief Get the char in an AString.
+ * @param string the source AString
+ * @param position the position you want to get
+ * @return the char in position
+ * @since 0.1
+ */
+char           a_string_get_char              (AString *string, asize position);
+/**
+ * @brief Set the char in an AString.
+ * @param string the source AString
+ * @param position the position you want to get
+ * @param the char you want to change to
+ * @return the char in position
+ * @since 0.1
+ */
+AString *      a_string_set_char              (AString *string, asize position, char ch);
+/**
+ * @brief Find a string from an AString
+ * @param string the source AString
+ * @param str the string you want to find
+ * @param asize the position where you want to find
+ * @return the position where the str at, return -1 when no found.
+ * @since 0.1
+ */
+int            a_string_find                  (AString *string,
+                                              char *str,
+                                              asize position);
+/**
+ * @brief Replace a string from an AString
+ * @param string the source AString
+ * @param findstr the string you want to find
+ * @param replacestr the string you want to replace
+ * @param asize the position where you want to replace
+ * @return the position where the str at, return -1 when no found.
+ * @since 0.1
+ */
+int            a_string_replace               (AString *string,
+                                              char *findstr,
+                                              char *replacestr,
+                                              asize position);
+/**
+ * @brief Replace all string from an AString
+ * @param string the source AString
+ * @param findstr the string you want to find
+ * @param replacestr the string you want to replace
+ * @return the count you have replace.
+ * @see a_string_replace
+ * @since 0.1
+ */
+int            a_string_replace_all           (AString *string,
+                                              char *findstr,
+                                              char *replacestr);
+/**
+ * @brief Frees the memory allocated for the AString.
+ * @param string the AString you want to free
+ * @since 0.1
+ */
 void           a_string_free                  (AString *string);
-
-/* calc a hash code */
+/**
+ * @brief Get the hashcode from a string.
+ * @param string the AString you want to hash
+ * @return the hashcode of the string
+ * @note using BKDRHash
+ * @since 0.1
+ */
 auint          a_string_hash                  (AString *string);
-
-/* return if two string is equal */
+/**
+ * @brief compare the two AStrings
+ * @param string the first AString
+ * @param string2 the second AString
+ * @return TRUE or FALSE
+ * @since 0.1
+ */
 aboolean       a_string_equal                 (AString *string,
                                               AString *string2);
 
 #endif
+
--- astringlist.h
+++ astringlist.h
@@ -1,90 +1,246 @@
+/**
+ * @file astringlist.h
+ * @brief AStringList
+ * @author Tom
+ * @date 2011-11-25
+ * @version 0.2
+ * @note
+ * A AString is an object that handles the memory management of a C string for you. You can think of it as similar to a Java StringBuffer. In addition to the string itself, GString stores the length of the string, so can be used for binary data with embedded nul bytes. To access the C string managed by the AString string, simply use string->str.
+ */
 #ifndef __A_STRING_LIST_H__
 #define __A_STRING_LIST_H__
-
 
 #include "astring.h"
 
 #define A_STRING_LIST_FLAG       102
-
 #define A_IS_STRING_LIST(string) (*(auchar *)string == A_STRING_LIST_FLAG)
 
-
+/**
+ * @brief AStringList struct
+ */
 struct _AStringList {
-	auchar             flag;
-	volatile aboolean  lock;
+	auchar             flag;  /**< Don't Change it, it used to check a object is AStringList. */
+	volatile aboolean  lock;  /**< Don't Change it, it used to keep thread safe. */
 
-	asize              length;
-	asize              allocated_len;
+	asize              length;  /**< contains the length of the stringlist. */
+	asize              allocated_len;  /**< contains the length of the stringlist. */
 
-	AString **     data;
+	AString **         data;  /**< points to the string data. */
 };
 
+/**
+ * @brief a function to be variable in qsort 
+ */
 typedef int (*A_CMP)(const void *a, const void *b);
+/**
+ * @brief StringList Main Struct 
+ */
 typedef struct _AStringList AStringList;
 
+/**
+ * @brief Create a new AStringList with none string
+ * @return A new AStringList
+ * @since 0.2
+ */
 AStringList *   a_string_list_new          (void);
-
+/**
+ * @brief Copies string from an AString into an AStringList, destroying any previous contents.
+ * It alias a_string_list_set_text;
+ * @param list the AStringList to opera
+ * @param string the AString to assign
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_assign       (AStringList *list,
                                             AString *string);
 
+/**
+ * @brief It's like assign,but,split will be \n.
+ * @param list the AStringList to opera
+ * @param string the AString to assign
+ * @param split the split string
+ * @return list
+ * @see a_string_list_assign
+ * @since 0.2
+ */
 AStringList *   a_string_list_assign_split (AStringList *list,
                                             AString *string,
                                             AString *split);
-
+/**
+ * @brief Create an AStringList from a file.
+ * @param filename the filename you want to load
+ * @return a new AStringList
+ * @since 0.2
+ */
 AStringList *   a_string_list_load_from_file(const char * filename);
-
+/**
+ * @brief Append AStringList to exists AStringList
+ * @param list the destination AStringList
+ * @param list2 the list you want to append
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_append       (AStringList *list,
                                             AStringList *list2);
-
+/**
+ * @brief Prepend AStringList to exists AStringList
+ * @param list the destination AStringList
+ * @param list2 the list you want to prepend
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_prepend      (AStringList *list,
                                             AStringList *list2);
-
+/**
+ * @brief Add an AString to the end of AStringList
+ * @param list the destination AStringList
+ * @param string the string you want to add
+ * @return list
+ * @see a_string_list_append
+ * @since 0.2
+ */
 AStringList *   a_string_list_add          (AStringList *list,
                                             AString *string);
-
+/**
+ * @brief Insert an AString to AStringList
+ * @param list the destination AStringList
+ * @param index the position you want to insert
+ * @param string the string you want to insert
+ * @return list
+ * @see a_string_list_add
+ * @since 0.2
+ */
 AStringList *   a_string_list_insert       (AStringList *list,
                                             asize index,
 											AString *string);
-
+/**
+ * @brief Delete an AString from an AStringList
+ * @param list the destination AStringList
+ * @param index the position you want to delete
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_delete       (AStringList *list,
                                             asize index);
-
+/**
+ * @brief Clear the AStringList
+ * @param list the AStringList you want to clear
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_clear        (AStringList *list);
-
+/**
+ * @brief Exchange two lines in an AStringList
+ * @param list the destination AStringList
+ * @param index1 the first position
+ * @param index2 the second position
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_exchange     (AStringList *list,
                                             asize index1,
 											asize index2);
-
+/**
+ * @brief Find an AString from an AStringList
+ * @param list An AStringList
+ * @param string the string to find
+ * @param index the position you want to find start
+ * @return the position, return -1 when doesn't find it.
+ * @since 0.2
+ */
 asize           a_string_list_find         (AStringList *list,
                                             AString *string,
 											asize index);
-
+/**
+ * @brief Find a key from an AStringList
+ *  When the stringlist is like key=value, find the key in the list
+ * @param list An AStringList
+ * @param string the string to find
+ * @param index the position you want to find start
+ * @return the position, return -1 when doesn't find it.
+ * @since 0.2
+ */
 asize			a_string_list_find_name    (AStringList *list,
                                             AString *string,
 											asize index);
-
+/**
+ * @brief Get a value from an AStringList
+ *  When the stringlist is like key=value, get the value from a string
+ * @param list An AStringList
+ * @param index the position you want to get value
+ * @return a new AString
+ * @see a_string_list_find_name
+ * @since 0.2
+ */
 AString *       a_string_list_get_value    (AStringList *list,
                                             asize index);
-
+/**
+ * @brief Sort the StringList
+ * @param list An AStringList 
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_sort         (AStringList *list);
-
+/**
+ * @brief Sort the StringList by a custom function
+ * @param list An AStringList
+ * @param cmp An Compare function
+ * @return list
+ * @since 0.2
+ */
 AStringList *   a_string_list_sort_custom  (AStringList *list,
 											A_CMP cmp);
-
+/**
+ * @brief Check the StringList is sorted
+ * @param list An AStringList 
+ * @return TRUE or FALSE
+ * @since 0.2
+ */
 aboolean        a_string_list_sorted       (AStringList *list);
-
+/**
+ * @brief duplicate the AStringList
+ * @warning You should free the return string when you don't want to use it.
+ * @param list An AStringList 
+ * @return a new AStringList
+ * @since 0.2
+ */
 AStringList *   a_string_list_dup          (AStringList *list);
-
-/* returnchar:0:\n,1:\r,2:\r\n */
+/**
+ * @brief export an AStringList to an AString
+ * @warning You should free the return string when you don't want to use it.
+ * @param list An AStringList
+ * @param returnchar the return char, 0:'\n', 1:'\r', 2:"\r\n"
+ * @return A new AString
+ * @since 0.2
+ */
 AString *       a_string_list_get_text     (AStringList *list, auchar returnchar);
-
+/**
+ * @brief export an AStringList to a file
+ * @warning You should free the return string when you don't want to use it.
+ * @param list An AStringList
+ * @param filename the file you want to export
+ * @param returnchar the return char, 0:'\n', 1:'\r', 2:"\r\n"
+ * @return TRUE or FALSE
+ * @since 0.2
+ */
 aboolean        a_string_list_save_to_file (AStringList *list,
                                             const char * filename,
                                             auchar returnchar);
-
+/**
+ * @brief compare the two AStringLists
+ * @param list the first AStringList
+ * @param list2 the second AStringList
+ * @return TRUE or FALSE
+ * @since 0.2
+ */
 aboolean        a_string_list_equal       (AStringList *list1,
                                             AStringList *list2);
-
+/**
+ * @brief Frees the memory allocated for the AStringList.
+ * @param string the AStringList you want to free
+ * @since 0.2
+ */
 void            a_string_list_free         (AStringList *list);
 
 #endif
+
