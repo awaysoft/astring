646d34f0d59786a80f41e1ff2b6ab78ad30744f3
Tom <tom@awaysoft.com>
2012-01-03 20:15:19 +0800

    Update Some Macros

--- atype.h
+++ atype.h
@@ -1,10 +1,17 @@
 #ifndef __ATYPE_H__
 #define __ATYPE_H__
 
-#define FALSE                0
-#define TRUE                 1
+#ifndef	FALSE
+#define	FALSE	(0)
+#endif
 
+#ifndef	TRUE
+#define	TRUE	(!FALSE)
+#endif
+
+#ifndef DEFAULT_SIZE
 #define DEFAULT_SIZE         100
+#endif
 
 typedef    unsigned long     asize;
 typedef    unsigned int      aunichar;
